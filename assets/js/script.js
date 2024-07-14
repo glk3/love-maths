d2b006f4f68023e0eb9c68ac8a0aeb634fc0dca2
Gediminas Luksevicius <gediminas30@yahoo.ie>
2024-07-14 08:36:51 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 /**
  * The main game "loop",called when the scrip is first loaded
  * And after the user's answer has bean processed
  */
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25. 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -30,7 +41,7 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
-    
+
 }
 
 function incrementScore() {
@@ -41,7 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1,operand2){
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
